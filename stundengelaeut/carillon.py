fd7e278c2bb6d456d003ecab8bbbc98531a63589
Matthias Gianfelice <matthias@gianfelice.de>
2020-02-29 08:40:55 +0100

    Carillon erweitert

--- stundengelaeut/carillon.py
+++ stundengelaeut/carillon.py
@@ -1,7 +1,16 @@
 class Carillon:
 
-    midi = None
+    midiout = None
+    channelAdder = None
 
-    def __init__(self):
-        # HIER: Initialisierung von MIDI
-        pass
+    def __init__(self, midiout, channel = 1):
+        self.midiout = midiout
+        self.channelAdder = channel - 1
+
+    def noteOn(self, note, velocity = 127):
+        msg = [0x90 + self.channelAdder, note, velocity]
+        self.midiout.send_message(msg)
+
+    def noteOff(self, note, velocity = 0):
+        msg = [0x80 + self.channelAdder, note, velocity]
+        self.midiout.send_message(msg)
