293b5da92e3f9add6b1b30e39dc45afc724f2d2b
Matthias Gianfelice <matthias@gianfelice.de>
2020-03-16 16:31:45 +0100

    Today-Manager zur Verwaltung des heutigen Tages.

---
+++ stundengelaeut/today.py
@@ -0,0 +1,72 @@
+from datetime import date
+from datetime import timedelta
+
+from stundengelaeut.festmanager import FestManager
+from stundengelaeut.feste.rang import Rang
+
+class Today:
+    """
+    Statische Methode, die Informationen zum heutigen Tage zusammenträgt und
+    gegebenenfalls aktualisiert.
+    """
+
+    """
+    Letzter Tag, an dem die Aktualisierung durchgeführt wurde. Wird
+    zwischengespeichert, damit nur einmal pro Tag geupdatet wird.
+    """
+    _lastupdate = date.today() + timedelta(days = -1)
+
+    """Aktuelle Antiphon, die wiedergegeben werden soll."""
+    _antiphon = None
+
+    """Fest, das am heutigen Tage begangen wird."""
+    _feast = None
+
+    @staticmethod
+    def check():
+        """
+        Überprüft, ob heute bereits ein Update stattgefunden hat. Falls nicht,
+        werden aktuellere Daten aus dem FestManager übernommen.
+        """
+        if Today._lastupdate >= date.today(): return
+        Today._lastupdate = date.today()
+        Today._antiphon = FestManager.antiphon()
+        Today._feast = FestManager.fest()
+
+    @staticmethod
+    def antiphon():
+        """
+        Gibt die aktuell zu spielende marianische Antiphon zurück.
+
+        Returns
+        -------
+        Die aktuell zu spielende Antiphon.
+        """
+        Today.check()
+        return Today._antiphon
+
+    @staticmethod
+    def feast():
+        """
+        Gibt das heute begangene Fest zurück.
+
+        Returns
+        -------
+        Das heute begangene Fest (oder eben None, wenn keines stattfindet).
+        """
+        Today.check()
+        return Today._feast
+
+    @staticmethod
+    def celebration():
+        """
+        Gibt zurück, ob heute ein Feiertag mit ausreichendem Rang begangen wird,
+        damit von dem normalen Läutschema abgewichen werden kann.
+
+        Returns
+        -------
+        Ob heute ein „begangenes“ Fest stattfindet.
+        """
+        Today.check()
+        if Today._feast == None: return False
+        return Today._feast.rang() >= Rang.NICHTGEBOTENER_GEDENKTAG
