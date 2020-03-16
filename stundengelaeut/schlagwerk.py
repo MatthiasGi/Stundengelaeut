c6db562d63a183bf8e12f6947c9bb918cc098de0
Matthias Gianfelice <matthias@gianfelice.de>
2020-03-16 12:20:53 +0100

    Schlagwerk und Datumsklasse begonnen.

--- stundengelaeut/schlagwerk.py
+++ stundengelaeut/schlagwerk.py
@@ -1,8 +1,27 @@
-import notes
+from stundengelaeut.notes import *
+from stundengelaeut import date
 
-BELL_TRINITAS = notes.A1SHARP
-BELL_MARIA    = notes.C2SHARP
-BELL_JOSEF    = notes.D2SHARP
-BELL_APOSTEL  = notes.F2SHARP
-BELL_BERNHARD = notes.G2SHARP
-BELL_ENGEL    = notes.A2SHARP
+class Schlagwerk:
+    def __init__(self, carillon):
+        self.carillon = carillon
+
+        self.trinitatis = A1SHARP
+        self.maria      = C2SHARP
+        self.josef      = D2SHARP
+        self.apostel    = F2SHARP
+        self.bernhard   = G2SHARP
+        self.engel      = A2SHARP
+
+    def tellQuarters(self, quarters):
+        for i in range(quarters):
+            if date.isCelebration():
+                self.carillon.playMelody([
+                    [0.5, self.engel],
+                    [0.5, self.bernhard],
+                    [1.5, self.apostel]
+                ])
+            else:
+                self.carillon.playMelody([[2, self.engel]])
+
+    def tellHours(self, hours):
+        for i in range(hours): self.carillon.playMelody([[3, self.trinitatis]])
