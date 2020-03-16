4d0696877de256f8f7352026f0550b053eb5ac23
Matthias Gianfelice <matthias@gianfelice.de>
2020-03-16 11:03:10 +0100

    Fehlende Festklassen hinzugefügt.

---
+++ stundengelaeut/feste/taufedesherrnfest.py
@@ -0,0 +1,17 @@
+from .deltafest import DeltaFest
+from datetime import date
+from datetime import timedelta
+
+class TaufeDesHerrnFest(DeltaFest):
+    """
+    Feste, die in Bezug auf das Fest „Taufe des Herrn“ stehen, werden hier
+    zusammengefasst. Taufe des Herrn fällt immer auf den Sonntag nach
+    Erscheinung des Herrn (6. Januar).
+    """
+
+    def referenceDate(self, year):
+        """@inheritDoc"""
+        erscheinung = date(year, 1, 6)
+        delta = 7 - erscheinung.isoweekday()
+        if delta == 0: delta = 7
+        return erscheinung + timedelta(days = delta)
---
+++ stundengelaeut/feste/weihnachtszeit.py
@@ -0,0 +1,27 @@
+from .taufedesherrnfest import TaufeDesHerrnFest
+from datetime import date
+
+class Weihnachtszeit(TaufeDesHerrnFest):
+    """
+    Die Weihnachtszeit beginnt mit dem Hochfest der Geburt (25.12., inklusive)
+    und endet mit dem Fest Taufe des Herrn (s. .taufedesherrn.TaufeDesHerrnFest,
+    inklusive). Aufgrund des Jahreswechsels ist hier auf Datumsüberschneidungen
+    zu achten.
+    """
+    def __init__(self):
+        pass
+
+    def isDate(self, d):
+        """@inheritDoc"""
+        if d.month == 12:
+            return date(d.year, 12, 25) <= d
+        else:
+            return self.referenceDate(d.year) >= d
+
+    def rang(self):
+        """@inheritDoc"""
+        return Rang.GEPRAEGTE_ZEIT
+
+    def name(self):
+        """@inheritDoc"""
+        return "Weihnchtszeit"
