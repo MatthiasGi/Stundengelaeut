62267a313a3a80d71235d56b9c18989b589db5e1
Matthias Gianfelice <matthias@gianfelice.de>
2020-05-18 19:46:59 +0200

    Fixed some bugs

--- rest.py
+++ rest.py
@@ -7,12 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 i2c = busio.I2C(board.SCL, board.SDA)
-tpa = adafruit_tpa2016(i2c)
+tpa = adafruit_tpa2016.TPA2016(i2c)
 
 @app.route('/state')
 def get_state():
-    return dict(state=not tpa.amplifier_shutdown)
+    return '{ state: %d }' % (1 if tpa.amplifier_shutdown else 0)
 
 @app.route('/state/<int:state>')
 def set_state(state):
     tpa.amplifier_shutdown = (state == 0)
+    return get_state()
